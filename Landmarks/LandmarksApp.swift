4cc4f0af5e47d0c6069ebdba61c4cb95c98f9943
donggeon Na <ndmb2012@gmail.com>
2024-07-07 09:19:45 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 나동건 on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
